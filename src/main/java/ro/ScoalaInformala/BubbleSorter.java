e8f0a9e14f3a29a6912af0cbb3afd87294ae3198
BotyB <botond_13@yahoo.com>
2025-03-28 21:26:54 +0200

    Created dev Branch + Completed BubbleSorter

--- src/main/java/ro/ScoalaInformala/BubbleSorter.java
+++ src/main/java/ro/ScoalaInformala/BubbleSorter.java
@@ -1,4 +1,22 @@
 package ro.ScoalaInformala;
 
-public class BubbleSorter {
+public class BubbleSorter { //Sort array of representatives based on revenue
+    public SalesRepresentative[] sort(SalesRepresentative[] representatives) {
+        if (representatives == null || representatives.length <= 1) {
+            return representatives;
+        }
+
+        int n = representatives.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (representatives[j].getRevenue() < representatives[j + 1].getRevenue()) {
+                    SalesRepresentative temp = representatives[j];
+                    representatives[j] = representatives[j + 1];
+                    representatives[j + 1] = temp;
+                }
+            }
+        }
+        return representatives;
+
+    }
 }
